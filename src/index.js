ba6178e16f61406f5c849ecd7127a55199d91e4b
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2022-01-27 05:15:05 +0200

    daone with some errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    console.log(` ---------- ALGORITM START ---------- `)
+    let OPEN_BRACKETS = bracketsConfig.map(el=>el[0])
+    let BRACKETS_PAIR = {};
+    let countSameBrackets = 0;
+    for (let i=0; i<bracketsConfig.length;i++){
+        let currKey = bracketsConfig[i][1]
+        let currValue = bracketsConfig[i][0]
+        BRACKETS_PAIR[currKey]=currValue;
+        if (currKey == currValue) {countSameBrackets +=1}
+    }
+    console.log(`string is ${str}`)
+    console.log(`OPEN_BRACKETS = ${OPEN_BRACKETS}`)
+    console.log(`BRACKETS_PAIR = `)
+    console.log(BRACKETS_PAIR)
+    console.log(`countSameBrackets = ${countSameBrackets}`)
+
+    
+
+    function isBracketsOk(str2) {
+        let stack = [];
+    
+        for (let i = 0; i < str2.length; i++) {
+          let currentSymbol = str2[i];
+          console.log(`currentSymbol = ${currentSymbol}`)
+          if (OPEN_BRACKETS.includes(currentSymbol)) {
+            console.log(`OPEN_BRACKETS includes currentSymbol = ${currentSymbol}`)
+            // case when same brackets
+            if (countSameBrackets>0) {
+                if (stack.length==0) {
+                    stack.push(currentSymbol)
+                } else {
+                    if ((stack[stack.length-1]==currentSymbol)) { 
+                        console.log(`key = value = |`);
+                        stack.pop()
+                    } else {stack.push(currentSymbol)}
+                } 
+            } 
+            // case when same brackets
+            else {stack.push(currentSymbol);}
+            
+            console.log(`OPEN_BRACKETS iteration close and stack is ${stack} `)
+          } else {
+            if (stack.length === 0) {
+              return false;
+            }
+    
+            let topElement = stack[stack.length - 1];
+    
+            if (BRACKETS_PAIR[currentSymbol] === topElement) {
+              stack.pop();
+            } else {
+              return false;
+            }
+          }
+        }
+        let isBracketsOkRes = (stack.length === 0)
+        console.log(stack)
+        console.log(isBracketsOkRes)
+        return isBracketsOkRes;
+    }
+    console.log(` ---------- ALGORITM END ---------- `)
+    return isBracketsOk (str);
+    
 }
